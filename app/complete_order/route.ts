1ad8f6e7934627d1a5c15048441db03d8a9b30cb
Dustin DeHaven <dustin.dehaven@gmail.com>
2024-03-18 19:28:20 +0900

    calculate tier along with total_spent WIP, works, but redundant

--- app/complete_order/route.ts
+++ app/complete_order/route.ts
@@ -53,16 +53,27 @@ export async function complete_order(completeOrderArgs: CompleteOrderArgs) {
     CREATE OR REPLACE FUNCTION update_customer_total_spent()
     RETURNS TRIGGER AS $$
     BEGIN
-        -- Calculate the total spent and update the customers table
-        UPDATE customers
-        SET total_spent = (
-            SELECT SUM(order_total_in_cents)
-            FROM orders
-            WHERE customer_id = NEW.customer_id
-        )
-        WHERE customer_id = NEW.customer_id;
+        -- Calculate the total spent 
         
+        WITH spent_calculation AS (
+          SELECT customer_id, SUM(order_total_in_cents) AS calculated_total_spent
+          FROM orders
+          GROUP BY customer_id
+        )
+        
+        -- Use calculated_total_spent to set field total_spent and determine current_tier 
+
+        UPDATE customers
+        SET total_spent = calculated_total_spent,
+            current_tier = CASE
+                WHEN calculated_total_spent BETWEEN 0 AND 9900 THEN 'BRONZE'
+                WHEN calculated_total_spent BETWEEN 100 AND 49900 THEN 'SILVER'
+                WHEN calculated_total_spent > 49900 THEN 'GOLD'
+            END
+        FROM spent_calculation
+        WHERE customers.customer_id = spent_calculation.customer_id;
         RETURN NEW;
+        
     END;
     $$ LANGUAGE plpgsql;
    */
