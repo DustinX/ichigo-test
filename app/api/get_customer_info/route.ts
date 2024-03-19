35c865849ec2c3c581b4518564dd5a3b85c5d235
Dustin DeHaven <dustin.dehaven@gmail.com>
2024-03-19 19:16:14 +0900

    add endpoint to get customer info

---
+++ app/api/get_customer_info/route.ts
@@ -0,0 +1,62 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  console.log("Start get_customer_info");
+
+  const { searchParams } = new URL(request.url);
+
+  const customer_id = searchParams.get("customer_id");
+
+  if (!customer_id) {
+    return NextResponse.json("customer_id not provided", { status: 400 });
+  }
+
+  try {
+    const customerInfoRaw = await get_customer_info(customer_id);
+    const customerInfo = customerInfoRaw.customer.rows;
+    console.log("Complete get_customer_info");
+    console.log(customerInfo);
+
+    return NextResponse.json({ customerInfo }, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ e }, { status: 500 });
+  }
+}
+
+export async function get_customer_info(customer_id: string) {
+  const customerInfo = await sql`
+      SELECT 
+        current_tier,
+        
+        date_trunc('year', CURRENT_DATE - INTERVAL '1 year') AS date_cutoff,
+        
+        total_spent,
+        
+      CASE
+        WHEN current_tier = 'BRONZE' THEN 10000 - total_spent
+        WHEN current_tier = 'SILVER' THEN 50000 - total_spent
+        WHEN current_tier = 'GOLD' THEN NULL
+      END  AS cents_till_next_tier,
+      
+      CASE
+        WHEN total_spent_this_year BETWEEN 0 AND 9999 THEN 'BRONZE'
+        WHEN total_spent_this_year BETWEEN 10000 AND 49999 THEN 'SILVER'
+        WHEN total_spent_this_year >= 50000 THEN 'GOLD'
+      END  AS tier_next_year,
+      
+      date_trunc('year', CURRENT_DATE + INTERVAL '1 year' - INTERVAL '1 second') AS downgrade_date,
+      
+      CASE
+        WHEN current_tier = 'BRONZE' THEN 5
+        WHEN current_tier = 'SILVER' THEN GREATEST(10000 - total_spent_this_year, 0)
+        WHEN current_tier = 'GOLD' THEN GREATEST(50000 - total_spent_this_year, 0)
+      END AS cents_to_maintain_tier_next_year
+      
+      FROM customers
+      WHERE customers.customer_id = ${customer_id};
+  `;
+
+  return { customer: customerInfo };
+}
